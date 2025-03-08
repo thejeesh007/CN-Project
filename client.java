2c896779d370469f5d1fc9ab850a0846b52f7f46
thejeesh007 <160516972+thejeesh007@users.noreply.github.com>
2025-03-09 00:08:59 +0530

    Update client.java

--- client.java
+++ client.java
@@ -3,14 +3,18 @@ import java.net.*;
 import java.util.*;
 
 class VideoClient {
+    private static final int HEARTBEAT_INTERVAL = 5000;
+    private static final int TIMEOUT = 10000;
+
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter relay port: ");
         int relayPort = scanner.nextInt();
-        scanner.nextLine();
+        scanner.nextLine(); // Consume newline
 
         Socket relaySocket = new Socket("localhost", relayPort);
+        relaySocket.setSoTimeout(TIMEOUT);
         System.out.println("Client connected to relay on port " + relayPort);
 
         DataInputStream in = new DataInputStream(relaySocket.getInputStream());
@@ -20,7 +24,7 @@ class VideoClient {
             try {
                 while (true) {
                     out.println("PING");
-                    Thread.sleep(5000);
+                    Thread.sleep(HEARTBEAT_INTERVAL);
                 }
             } catch (Exception e) {
                 System.out.println("Heartbeat stopped.");
@@ -32,19 +36,26 @@ class VideoClient {
         scanner.nextLine();
 
         out.println(numPackets);
+        Set<Integer> missingPackets = new HashSet<>();
 
         for (int i = 0; i < numPackets; i++) {
-            System.out.println("Client: Requesting packet " + i);
             out.println(i);
-            String packet = in.readUTF();
-
-            if (packet.equals("NACK")) {
-                System.out.println("Client: NACK Received for packet " + i);
-                i--; // Request again
-                continue;
+            try {
+                String packet = in.readUTF();
+                if (packet.equals("NACK")) {
+                    missingPackets.add(i);
+                } else {
+                    System.out.println("Client: Received " + packet);
+                }
+            } catch (IOException e) {
+                missingPackets.add(i);
             }
+        }
 
-            System.out.println("Client: Received " + packet);
+        for (int index : missingPackets) {
+            out.println("NACK " + index);
+            String packet = in.readUTF();
+            System.out.println("Client: Retransmitted " + packet);
         }
 
         relaySocket.close();
